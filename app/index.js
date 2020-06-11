4fa54a3c591478411751b8b04602353c37525a04
Chaiwtun <chaiwtun@scg.com>
2020-06-11 10:13:46 +0700

    [master] init simple http server app with node

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-console.log('Hello Max');
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer(function accessRoute(_, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write("Hello Max");
+  response.end();
+})
+
+server.listen(port, function () {
+  console.log('App listen on port:', port);
+});
